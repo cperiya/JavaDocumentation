16688669638e04399ac8698ae60c9cee4d20e85b
cperiya <157152157+cperiya@users.noreply.github.com>
2024-04-01 16:58:00 -0400

    java document generated

--- src/Shape.java
+++ src/Shape.java
@@ -1,14 +1,20 @@
-/**An abstract class representing various kinds of shapes. */
+/**An abstract class representing various kinds of shapes.
+ * @author Chitra Periya 
+ * @version v2024.03.1*/
 
 public abstract class Shape {
 
-	/** returns the x-coordinate of this shape to the argument*/
+	/** returns the x-coordinate of this shape to the argument
+	 * @return value of X*/
 	public void getX() {}
 
 	/** set the x-coordinate of this shape in the argument
 	 * <code>x</code>
+	 * @param X value passed  
 	 */
 	public void setX(int X) {}
 	
-	public static void main(String[] args) {}
+	public static void main(String[] args) {
+		
+	}
 }
